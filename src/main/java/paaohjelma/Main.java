6003ee2ecba01771206b5c957914f3f9845d5692
Eevi Bengs <eevi.bengs@helsinki.fi>
2021-02-08 15:50:53 +0200

    changes to break chekstyle

--- src/main/java/paaohjelma/Main.java
+++ src/main/java/paaohjelma/Main.java
@@ -12,6 +12,17 @@ public class Main {
         System.out.println("Luonnin jälkeen:");
         System.out.println("Mehuvarasto: " + mehua);
         System.out.println("Olutvarasto: " + olutta);
+
+        System.out.println("Luonnin jälkeen:");
+        System.out.println("Mehuvarasto: " + mehua);
+        System.out.println("Olutvarasto: " + olutta);
+
+        System.out.println("Luonnin jälkeen:");
+        System.out.println("Mehuvarasto: " + mehua);
+        System.out.println("Olutvarasto: " + olutta);
+        System.out.println("Luonnin jälkeen:");
+        System.out.println("Mehuvarasto: " + mehua);
+        System.out.println("Olutvarasto: " + olutta);
         
     }
 }
